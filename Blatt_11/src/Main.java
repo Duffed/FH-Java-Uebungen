3df8c5eb2d591ccffc96fb77bf074a2724cafd5d
Joscha Diederichs <joschadiederichs@Joschas-MacBook-Pro.local>
2016-12-19 10:31:17 +0100

    Fix

--- Blatt_11/src/Main.java
+++ Blatt_11/src/Main.java
@@ -1,10 +1,11 @@
+import javax.swing.*;
+
 /**
  * Created by fr3 on 14.12.2016.
  */
-import javax.swing.*;
-import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String zahl1 = JOptionPane.showInputDialog("Zahl 1:");
         String zahl2 = JOptionPane.showInputDialog("Zahl 2:");
         int ergebnislaenge = 0;
@@ -19,10 +20,10 @@ public class Main {
         int[] zahl2A = new int[zahl2.length()];
 
         //int arrays mit Zahlen füllen
-        for (int i = 0; i < zahl1A.length; i++){
+        for (int i = 0; i < zahl1A.length; i++) {
             zahl1A[i] = Integer.parseInt(String.valueOf(zahl1.charAt(i)));
         }
-        for (int i = 0; i < zahl2A.length; i++){
+        for (int i = 0; i < zahl2A.length; i++) {
             zahl2A[i] = Integer.parseInt(String.valueOf(zahl2.charAt(i)));
         }
 
@@ -32,16 +33,16 @@ public class Main {
         } else {
             ergebnislaenge = zahl2A.length;
         }
-        int[] ergebnis = new int[ergebnislaenge+1];
+        int[] ergebnis = new int[ergebnislaenge + 1];
 
         //berechnung
         int imSinn = 0;
         int zwischensumme = 0;
-        int z1Laenge = zahl1A.length-1;
-        int z2Laenge = zahl2A.length-1;
+        int z1Laenge = zahl1A.length - 1;
+        int z2Laenge = zahl2A.length - 1;
 
 
-        while(ergebnislaenge >= 0) {
+        while (ergebnislaenge >= 0) {
             int z1 = 0;
             int z2 = 0;
 
@@ -53,10 +54,10 @@ public class Main {
             }
 
             zwischensumme = z1 + z2 + imSinn;
-            imSinn = zwischensumme/10;
+            imSinn = zwischensumme / 10;
             zwischensumme %= 10;
 
-            ergebnis[ergebnislaenge] = zwischensumme ;
+            ergebnis[ergebnislaenge] = zwischensumme;
 
             z1Laenge--;
             z2Laenge--;
@@ -65,7 +66,7 @@ public class Main {
 
 
         //ausgabe
-        for(int i = 0; i < ergebnis.length; i++){
+        for (int i = 0; i < ergebnis.length; i++) {
             System.out.print(ergebnis[i]);
         }
 
